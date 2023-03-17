4acf838d8f5b00ebb41e5f8d02e9dcccc28dff71
aaronespasa <aaespasa@gmail.com>
2023-03-17 14:06:01 +0100

    🔧 Easing the development of the tests

--- cliente.c
+++ cliente.c
@@ -28,51 +28,8 @@ void test_init(unsigned long thread_num)
     }
 }
 
-void test_set_value(unsigned long thread_num)
+void test_set_value(int key, char value1[256], int value2, double value3, unsigned long thread_num)
 {
-    int key = 0;
-    char value1[256] = "Hola mundo";
-    int value2 = 1234;
-    double value3 = 3.1416;
-
-    int ret = set_value(key, value1, value2, value3);
-
-    if (ret == 0)
-    {
-        printf("(set_value - %lu):     Se guardó satisfactoriamente el valor (%s, %d, %f) en la clave %d\n", thread_num, value1, value2, value3, key);
-    }
-    else
-    {
-        printf("(set_value - %lu):     Error (código %d) al guardar el valor (%s, %d, %f) en la clave %d\n", thread_num, ret, value1, value2, value3, key);
-    }
-}
-
-void test_set_value1(unsigned long thread_num)
-{
-    int key = 3;
-    char value1[256] = "Hola mundo";
-    int value2 = 1234;
-    double value3 = 3.1416;
-
-    int ret = set_value(key, value1, value2, value3);
-
-    if (ret == 0)
-    {
-        printf("(set_value - %lu):     Se guardó satisfactoriamente el valor (%s, %d, %f) en la clave %d\n", thread_num, value1, value2, value3, key);
-    }
-    else
-    {
-        printf("(set_value - %lu):     Error (código %d) al guardar el valor (%s, %d, %f) en la clave %d\n", thread_num, ret, value1, value2, value3, key);
-    }
-}
-
-void test_set_value2(unsigned long thread_num)
-{
-    int key = 2;
-    char value1[256] = "Hola mundo";
-    int value2 = 13114;
-    double value3 = 3.23123;
-
     int ret = set_value(key, value1, value2, value3);
 
     if (ret == 0)
@@ -106,29 +63,22 @@ void test_get_value(int key, unsigned long thread_num)
     free(value3);
 }
 
-void test_modify_value(unsigned long thread_num)
+void test_modify_value(int key, char value1_modified[256], int value2, double value3, unsigned long thread_num)
 {
-    int key = 2;
-    char value1[256] = "Hola jefe";
-    int value2 = 4321;
-    double value3 = 6.2832;
-
-    int ret = modify_value(key, value1, value2, value3);
+    int ret = modify_value(key, value1_modified, value2, value3);
 
     if (ret == 0)
     {
-        printf("(modify_value - %lu):  Se modificó satisfactoriamente la clave %d con el nuevo valor (%s, %d, %f)\n", thread_num, key, value1, value2, value3);
+        printf("(modify_value - %lu):  Se modificó satisfactoriamente la clave %d con el nuevo valor (%s, %d, %f)\n", thread_num, key, value1_modified, value2, value3);
     }
     else
     {
-        printf("(modify_value - %lu):  Error (código %d) al modificar la clave %d con el nuevo valor (%s, %d, %f)\n", thread_num, ret, key, value1, value2, value3);
+        printf("(modify_value - %lu):  Error (código %d) al modificar la clave %d con el nuevo valor (%s, %d, %f)\n", thread_num, ret, key, value1_modified, value2, value3);
     }
 }
 
-void test_delete_key(unsigned long thread_num)
+void test_delete_key(int key, unsigned long thread_num)
 {
-    int key = 0;
-
     int ret = delete_key(key);
 
     if (ret == 0)
@@ -141,10 +91,8 @@ void test_delete_key(unsigned long thread_num)
     }
 }
 
-void test_exist(unsigned long thread_num)
+void test_exist(int key, unsigned long thread_num)
 {
-    int key = 0;
-
     int ret = exist(key);
 
     if (ret == 1)
@@ -161,11 +109,8 @@ void test_exist(unsigned long thread_num)
     }
 }
 
-void test_copy_key(unsigned long thread_num)
+void test_copy_key(int key1, int key2, unsigned long thread_num)
 {
-    int key1 = 0;
-    int key2 = 1;
-
     int ret = copy_key(key1, key2);
 
     if (ret == 0)
@@ -177,33 +122,6 @@ void test_copy_key(unsigned long thread_num)
         printf("(copy_key - %lu):      Error (código %d) al copiar la clave %d en la clave %d\n", thread_num, ret, key1, key2);
     }
 }
-
-void test_copy_key1(unsigned long thread_num)
-{
-    int key1 = 0;
-    int key2 = 2;
-
-    int ret = copy_key(key1, key2);
-
-    if (ret == 0)
-    {
-        printf("(copy_key - %lu):      La clave %d se copio satisfactoriamente en la clave %d\n", thread_num, key1, key2);
-    }
-    else
-    {
-        printf("(copy_key - %lu):      Error (código %d) al copiar la clave %d en la clave %d\n", thread_num, ret, key1, key2);
-    }
-}
-
-// test_modify_value(thread_num);
-// test_delete_key(thread_num);
-// test_exist(thread_num);
-
-// test_set_value(thread_num);
-// test_exist(thread_num);
-// test_copy_key(thread_num);
-
-// test_copy_key1(thread_num);
 
 void call_test(thread_data* th_data)
 {
@@ -220,11 +138,22 @@ void call_test(thread_data* th_data)
     busy = false;
     pthread_cond_signal(&init_finished);
     pthread_mutex_unlock(&protect_init_mutex);
-    
-    test_set_value(th_data_copy.thread_num);
-    test_set_value1(th_data_copy.thread_num);
-    test_set_value2(th_data_copy.thread_num);
+
+    char value1[256] = "Hola mundo";
+    char value1_modified[256] = "Hola jefe";
+
+    test_set_value(0, value1, 1234, 3.1416, th_data_copy.thread_num);
+    test_set_value(3, value1, 1234, 3.1416, th_data_copy.thread_num);
+    test_set_value(2, value1, 13114, 3.23123, th_data_copy.thread_num);
     test_get_value(2, th_data_copy.thread_num);
+
+    test_modify_value(2, value1_modified, 4321, 6.2832, th_data_copy.thread_num);
+    test_delete_key(0, th_data_copy.thread_num);
+    test_exist(0, th_data_copy.thread_num);
+    test_set_value(0, value1, 1234, 3.1416, th_data_copy.thread_num);
+    test_exist(0, th_data_copy.thread_num);
+    test_copy_key(0, 1, th_data_copy.thread_num);
+    test_copy_key(0, 2, th_data_copy.thread_num);
 }
 
 int main()
